18495c8a1abb00ce0cc9a685ba0294f8d6251325
fe1o <luizfcsp80@gmail.com>
2024-02-12 22:27:35 -0300

    Added user edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -7,14 +9,31 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Account was created successfully"
-      redirect_to articles_path
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    if  @user.update(user_params)
+      flash[:notice] = "Account was updated successfully."
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
